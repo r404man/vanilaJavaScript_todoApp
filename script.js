cb6a190f436ea6b95d5fd1d3ad51702a56317cc9
Roman Tyrtychko <tyrtychko96@gmail.com>
2020-11-23 12:30:48 +0600

    Add remove btn and some styles

--- script.js
+++ script.js
@@ -2,19 +2,51 @@ const addBtn = document.getElementById('todo-btn');
 var todoText = document.getElementById('todo-text');
 var todoList = document.getElementById('todo-list');
 var idTask = 0;
-addBtn.addEventListener('click', function() { 
-    let task = document.createElement('div');
-    task.className = "task";
-    task.setAttribute('id', `${idTask++}`);
+var taskDate = new Date();
+
+addBtn.addEventListener('click', function () {
+    if (todoText.value == "") {
+        alert('Plz insert data !');
+        todoText.style.border = `1px solid red`;
+    }
+    else {
+        // add main task 
+        let task = document.createElement('div');
+        task.className = "task";
+        task.setAttribute('id', `${idTask++}`);
+
+        // add task text
+        let taskText = document.createElement('div');
+        taskText.className = "taskText";
+        taskText.setAttribute('id', 'taskText');
+        taskText.innerHTML = `${todoText.value}`;
+        task.appendChild(taskText);
 
 
-    let taskText = document.createElement('div');
-    taskText.className = "taskText";
-    taskText.setAttribute('id', 'taskText');
-    taskText.innerHTML = `${todoText.value}`;
-    task.appendChild(taskText);
+        // add remove task button 
+
+        let removeTask = document.createElement('div');
+        removeTask.setAttribute('id', 'removeTask');
+        removeTask.className = "removeTask";
+        task.appendChild(removeTask);
+
+        removeTask.addEventListener('click', function () {
+            todoList.removeChild(task);
+        });
 
 
-    todoList.appendChild(task);
+
+        task.addEventListener('contextmenu', function () {
+            todoList.removeChild(this);
+            console.log(this);
+        });
+
+        todoList.appendChild(task);
+        
+        todoText.value = "";
+    }
+
+
+
 });
 
